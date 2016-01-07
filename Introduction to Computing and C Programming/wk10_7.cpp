4909144327d28bebbbd8a6f43b463d3ff7174ca9
Shen Zhu <shenzhu0127@gmail.com>
2016-01-07 15:17:06 +0800

    fix "find median" problem

--- Introduction to Computing and C Programming/wk10_7.cpp
+++ Introduction to Computing and C Programming/wk10_7.cpp
@@ -5,42 +5,42 @@ int main(){
 	int N;
 	int iNumbers[15000];
 
-	//cin
-	cin >> N;
-	if (N == 0){
-		//cout << 0 << endl;
-		return 0;
-	}
-	for (int i = 0; i < N; i++){
-		cin >> iNumbers[i];
-	}
-
-	//sort
-	for (int i = 0; i<N - 1; i++)
+	while (cin >> N)
 	{
-		int k = i;
-		int key = iNumbers[i];
-		for (int j = i + 1; j<N; j++)
+		if (N == 0){
+			//cout << 0 << endl;
+			return 0;
+		}
+		for (int i = 0; i < N; i++){
+			cin >> iNumbers[i];
+		}
+
+		//sort
+		for (int i = 0; i<N - 1; i++)
 		{
-			if (iNumbers[j]<key)
+			int k = i;
+			int key = iNumbers[i];
+			for (int j = i + 1; j<N; j++)
 			{
-				k = j;
-				key = iNumbers[j];
+				if (iNumbers[j]<key)
+				{
+					k = j;
+					key = iNumbers[j];
+				}
+			}
+			if (k != i){
+				int temp = iNumbers[i];
+				iNumbers[i] = iNumbers[k];
+				iNumbers[k] = temp;
 			}
 		}
-		if (k != i){
-			int temp = iNumbers[i];
-			iNumbers[i] = iNumbers[k];
-			iNumbers[k] = temp;
+
+		//cout
+		if (N % 2 == 0){
+			cout << (iNumbers[N / 2] + iNumbers[N / 2 - 1]) / 2 << endl;
+		}
+		else{
+			cout << iNumbers[N / 2] << endl;
 		}
 	}
-
-	//cout
-	if (N % 2 == 0){
-		cout << (iNumbers[N / 2] + iNumbers[N / 2 - 1]) / 2 << endl;
-	}
-	else{
-		cout << iNumbers[N / 2] << endl;
-	}
-	return 0;
 }
